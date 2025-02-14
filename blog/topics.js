a6b4d6cef8a8a286132cbc8d409635ffeb37ef7e
The Rainbow Chaos System <101984939+chocolate-pancake@users.noreply.github.com>
2025-02-14 14:08:54 +0100

    One more topic for the blog

---
+++ blog/topics.js
@@ -0,0 +1,149 @@
+let notPluralityHidden = false
+
+function plurality(){
+    let elements = document.querySelectorAll(".not-plurality");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notPluralityHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notPluralityHidden = !notPluralityHidden
+}
+
+let notDissociationHidden = false
+
+function dissociation(){
+    let elements = document.querySelectorAll(".not-dissociation");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notDissociationHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notDissociationHidden = !notDissociationHidden
+}
+
+let notTraumaHidden = false
+
+function trauma(){
+    let elements = document.querySelectorAll(".not-trauma");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notTraumaHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notTraumaHidden = !notTraumaHidden
+}
+
+let notLGBTHidden = false
+
+function lgbt(){
+    let elements = document.querySelectorAll(".not-lgbt");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notLGBTHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notLGBTHidden = !notLGBTHidden
+}
+
+let notReligionHidden = false
+
+function religion(){
+    let elements = document.querySelectorAll(".not-religion");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notReligionHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notReligionHidden = !notReligionHidden
+}
+
+let notEducationHidden = false
+
+function education(){
+    let elements = document.querySelectorAll(".not-education");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notEducationHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notEducationHidden = !notEducationHidden
+}
+
+let notActivismHidden = false
+
+function activism(){
+    let elements = document.querySelectorAll(".not-activism");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notActivismHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notActivismHidden = !notActivismHidden
+}
+
+let notReflectionsHidden = false
+
+function reflections(){
+    let elements = document.querySelectorAll(".not-reflections");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notReflectionsHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notReflectionsHidden = !notReflectionsHidden
+}
+
+let notExperiencesHidden = false
+
+function experiences(){
+    let elements = document.querySelectorAll(".not-experiences");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notExperiencesHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notExperiencesHidden = !notExperiencesHidden
+}
+
+let notAnnouncementsHidden = false
+
+function announcements(){
+    let elements = document.querySelectorAll(".not-announcements");
+    for(let i = 0; i < elements.length; i ++){
+        if(!notAnnouncementsHidden){
+            elements[i].style.display = "none";
+        }
+        else{
+            elements[i].style.display = "block";
+        }
+    }
+    notAnnouncementsHidden = !notAnnouncementsHidden
+}
